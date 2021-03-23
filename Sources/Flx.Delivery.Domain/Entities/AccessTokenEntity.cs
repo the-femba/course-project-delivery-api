0f099bbe41ac25b4beebf8e3b05cb5d7eef0f4cc
Roman S <ua.romansyslikov@gmail.com>
2021-03-23 18:59:54 +0300

    Add AccessTokenEntity And Add UserEntity

---
+++ Sources/Flx.Delivery.Domain/Entities/AccessTokenEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Domain.Entities
+{
+    public sealed class AccessTokenEntity
+    {
+        public string Token { get; set; } = null!;
+    }
+}
---
+++ Sources/Flx.Delivery.Domain/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using Rovecode.Lotos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Domain.Entities
+{
+    public sealed class UserEntity : StorageEntity<UserEntity>
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        // pref sha256 string
+        public string PasswordHash { get; set; } = null!;
+    }
+}
