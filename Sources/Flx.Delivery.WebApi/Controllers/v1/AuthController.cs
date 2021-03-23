26dae64c272447770c61e1abda603bc327240f12
Roman S <ua.romansyslikov@gmail.com>
2021-03-24 02:43:56 +0300

    Add UserController And Add AuthController

---
+++ Sources/Flx.Delivery.WebApi/Controllers/v1/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.WebApi.Controllers.v1
+{
+    [ApiController]
+    [Route("v1/auth/")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("singIn")]
+        public async Task<IActionResult> PostRegistrateUser([FromBody] Application.Microservices.Commands.RegistrateUserCommand.Command command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpPost("singUp")]
+        public async Task<IActionResult> PostLoginUser([FromBody] Application.Microservices.Commands.LoginUserCommand.Command command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
---
+++ Sources/Flx.Delivery.WebApi/Controllers/v1/UserController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.WebApi.Controllers.v1
+{
+    [ApiController]
+    [Route("v1/user/")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("info")]
+        public async Task<IActionResult> GetRegistrateUser()
+        {
+            var query = new Application.Microservices.Queries.GetCurrentUserInformationQuery.Query();
+
+            return Ok(await _mediator.Send(query));
+        }
+    }
+}
