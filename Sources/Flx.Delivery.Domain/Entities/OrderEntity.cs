ceae73d4db9b839539fc2b8721b690f9bec79a22
Roman S <ua.romansyslikov@gmail.com>
2021-03-25 21:33:34 +0300

    Add OrderEntity

---
+++ Sources/Flx.Delivery.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,27 @@
+﻿using Flx.Delivery.Domain.Enums;
+using Rovecode.Lotos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Domain.Entities
+{
+    public sealed class OrderEntity : StorageEntity<OrderEntity>
+    {
+        public DateTime CreateDate { get; set; }
+
+        public DateTime? CloseDate { get; set; }
+
+        public Guid RestaurantId { get; set; }
+
+        public Guid СustomerId { get; set; }
+
+        public Guid CourierId { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public IEnumerable<Guid> OrderFoodsIds { get; set; } = new Guid[] { };
+    }
+}
