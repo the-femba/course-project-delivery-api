64ba2a93b0ef495018890c8e59460e812cec07fe
Roman S <ua.romansyslikov@gmail.com>
2021-03-25 21:24:35 +0300

    Add RestaurantEntity

---
+++ Sources/Flx.Delivery.Domain/Entities/RestaurantEntity.cs
@@ -0,0 +1,31 @@
+﻿using Rovecode.Lotos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Domain.Entities
+{
+    public sealed class RestaurantEntity : StorageEntity<RestaurantEntity>
+    {
+        public sealed class RestaurantInformation
+        {
+            public string Name { get; set; } = null!;
+            public string Description { get; set; } = null!;
+        }
+
+        public sealed class RestaurantPhotos
+        {
+            public string LogoPhotoName { get; set; } = null!;
+            public string BackwardPhotoName { get; set; } = null!;
+        }
+
+        public RestaurantInformation Information { get; set; } = new();
+        public RestaurantPhotos Photos { get; set; } = new();
+
+        public IEnumerable<Guid> FoodsIds { get; set; } = new Guid[] { };
+
+        public IEnumerable<Guid> RestaurateursIds { get; set; } = new Guid[] { };
+    }
+}
--- Sources/Flx.Delivery.Domain/Enums/RoleType.cs
+++ Sources/Flx.Delivery.Domain/Enums/RoleType.cs
@@ -3,7 +3,6 @@
     public enum RoleType
     {
         Admin = 0,
-        Restaurateur = 1,
         Courier = 2,
         User = 3,
     }
