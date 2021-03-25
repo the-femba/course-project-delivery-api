344b18ebcdfc8f0aacc62963c2f45a6bc5661007
Roman S <ua.romansyslikov@gmail.com>
2021-03-25 21:27:22 +0300

    Add FoodEntity

---
+++ Sources/Flx.Delivery.Domain/Entities/FoodEntity.cs
@@ -0,0 +1,18 @@
+﻿using Rovecode.Lotos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Domain.Entities
+{
+    public sealed class FoodEntity : StorageEntity<FoodEntity>
+    {
+        public string FoodPhotoName { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public double PriceGrn { get; set; }
+    }
+}
