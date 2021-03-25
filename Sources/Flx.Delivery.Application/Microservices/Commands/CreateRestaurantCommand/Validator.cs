18440cda0e1f73553321113925422416c9a718bc
Roman S <ua.romansyslikov@gmail.com>
2021-03-25 21:51:56 +0300

    Addd OneResult

---
+++ Sources/Flx.Delivery.Application/Microservices/Commands/CreateRestaurantCommand/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Application.Microservices.Commands.CreateRestaurantCommand
+{
+    class Validator
+    {
+    }
+}
---
+++ Sources/Flx.Delivery.Application/Models/OneResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Application.Models
+{
+    public sealed class OneResult<T>
+    {
+        public T? Result { get; set; }
+    }
+}
