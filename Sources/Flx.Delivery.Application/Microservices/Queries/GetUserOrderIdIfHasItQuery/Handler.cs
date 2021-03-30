0493de622168129414e649555ccb0797b633830f
Roman S <ua.romansyslikov@gmail.com>
2021-03-30 18:54:53 +0300

    Add GetUserOrderIdIfHasItQuery

---
+++ Sources/Flx.Delivery.Application/Microservices/Queries/GetUserOrderIdIfHasItQuery/Handler.cs
@@ -0,0 +1,46 @@
+﻿using Flx.Delivery.Application.Interfaces.Accessors;
+using Flx.Delivery.Application.Interfaces.Repositories;
+using Flx.Delivery.Domain.Entities;
+using MediatR;
+using Rovecode.Lotos.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Application.Microservices.Queries.GetUserOrderIdIfHasItQuery
+{
+    public sealed class Handler : IRequestHandler<Query, Result>
+    {
+        private readonly IStorage<OrderEntity> _orderStorage;
+        private readonly IUserEntityStorage _userEntityStorage;
+        private readonly IHttpAuthAccessor _authAccessor;
+
+        public Handler(IHttpAuthAccessor authAccessor, IUserEntityStorage userEntityStorage, IStorage<OrderEntity> orderStorage)
+        {
+            _userEntityStorage = userEntityStorage;
+            _orderStorage = orderStorage;
+            _authAccessor = authAccessor;
+        }
+
+        public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+        {
+            string token = _authAccessor.AccessToken!;
+            UserEntity user = (await _userEntityStorage.PickViaAccessToken(token))!;
+
+            var order = await _orderStorage.Pick(e => e.CustomerId == user.Id && e.Status >= 0);
+
+            if (order is null)
+            {
+                return new();
+            }
+
+            return new()
+            {
+                OrderId = order.Id,
+            };
+        }
+    }
+}
---
+++ Sources/Flx.Delivery.Application/Microservices/Queries/GetUserOrderIdIfHasItQuery/Query.cs
@@ -0,0 +1,17 @@
+﻿using Flx.Delivery.Application.Attributes;
+using Flx.Delivery.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flx.Delivery.Application.Microservices.Queries.GetUserOrderIdIfHasItQuery
+{
+    [Auth(RoleType.User)]
+    public sealed class Query : IRequest<Result>
+    {
+
+    }
+}
